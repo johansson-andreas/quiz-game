6b578a0159be903455ac07fbdecd002a8fe7df8a
Andreas <nevarrix@gmail.com>
2024-07-03 07:33:01 +0200

    stuff

--- frontend/src/components/Socket.js
+++ frontend/src/components/Socket.js
@@ -1,6 +1,6 @@
 import io from 'socket.io-client';
 
-const socket = io('192.168.50.95:4000', {
+const socket = io('192.168.1.250:4000', {
   transports: ['websocket'], 
   withCredentials: true
 }); 
