3c88d1169f637d1bf877d83bd5484860eb4b4f8d
Andreas <nevarrix@gmail.com>
2024-07-15 14:52:12 +0200

    Restructure of initialContact endpoint and catching errors with undefined clientdata

--- backend/routes/questionRoutes.js
+++ backend/routes/questionRoutes.js
@@ -1,29 +1,34 @@
 import express from 'express';
-import { getNewQuestion, obfQuestion, updateScoreArray, getNewQuestionQueueByTags, shuffleArray } from './routesUtils.js';
+import { getNewQuestion, obfQuestion, updateScoreArray, getNewQuestionQueueByTags, shuffleArray, createClientData } from './routesUtils.js';
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-  // Placeholder route handler, add your implementation if needed
-});
+const timeLog = async (req, res, next) => {
+  if(!req.session.clientData) {
+    await createClientData(req);
+    await getNewQuestion(req.session.clientData);
+    next();
+  }
+  else next();
+}
+router.use(timeLog)
 
-router.get('/requestQuestion', async (req, res) => {
+router.get('/requestQuestion', async (req, res, next) => {
   const clientData = req.session.clientData;
   res.send(obfQuestion(await getNewQuestion(clientData)));
 });
 
 router.post('/submitAnswer', async (req, res) => {
   const clientData = req.session.clientData;
-  const answer = req.body.submittedAnswer;
-  console.log('Received answer', answer, 'from controller client', clientData.clientId);
-  updateScoreArray(clientData, answer);
-  console.log({ scoreArray: clientData.currentScores, correctAnswer: clientData.currentQuestion.correctAnswer });
-  res.send({ scoreArray: clientData.currentScores, correctAnswer: clientData.currentQuestion.correctAnswer });
+  if(clientData) {
+    const answer = req.body.submittedAnswer;
+    updateScoreArray(clientData, answer);
+    res.send({ scoreArray: clientData.currentScores, correctAnswer: clientData.currentQuestion.correctAnswer });
+  }
 });
 
 router.post('/getNewQuestionQueueByTags', async (req, res) => {
   const clientData = req.session.clientData;
-  console.log(req.body.questionCategories);
 
   clientData.categories = req.body.questionCategories;
 
