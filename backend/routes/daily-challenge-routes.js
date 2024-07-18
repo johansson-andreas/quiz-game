26c26255b6bdd4f5536ba0a1ff2c2d12718e6f82
Andreas <nevarrix@gmail.com>
2024-07-18 18:22:37 +0200

    Made dailychallengeroute require being logged in and added first iteration of a user history panel. Fixed an impure function in SingePlayer

--- backend/routes/daily-challenge-routes.js
+++ backend/routes/daily-challenge-routes.js
@@ -1,73 +1,83 @@
 import express from 'express';
-import { DailyChallengeQuestions, generateNewQuestions} from '../models/DailyChallengeQuestions.js';
-import { getNewQuestion, obfQuestion, updateDatabaseDailyChallengeScore} from './dailyChallengeRouteUtils.js';
-import {getQuestionCategories } from './questionRouteUtils.js'
-  
+import { DailyChallengeQuestions, generateNewQuestions } from '../models/DailyChallengeQuestions.js';
+import { getNewQuestion, obfQuestion, updateDatabaseDailyChallengeScore } from './dailyChallengeRouteUtils.js';
+import { getQuestionCategories } from './questionRouteUtils.js'
+import { DailyScore } from '../models/DailyScore.js';
+
 const router = express.Router();
 
 
 router.get('/initial-contact', async (req, res, next) => {
-    try {
-        //TODO: GRAB DAILY DATA FROM THE DB AND SET IT AS THE SESSION DATA. IF NO DAILY DATA CREATE NEW SESSION DATA
-        
-        // Check if there is no session data or if the date in session data is different from today
-        //TODO: CHANGE TO CHECKING DATABASE FOR ACCOUNT INSTEAD OF USING SESSION DATA
-        if (!req.session.dailyChallengeData) {
-            const today = new Date(); //Setting todays time to 0s as MongoDB's Date field will always include a time (which we set to 0s when we created todays document)
-            today.setHours(0, 0, 0, 0);
-            let todaysQuestions = await DailyChallengeQuestions.findOne({ date: today}, 'questionIDs').lean();
+  try {
+    //TODO: GRAB DAILY DATA FROM THE DB AND SET IT AS THE SESSION DATA. IF NO DAILY DATA CREATE NEW SESSION DATA
 
-            if (!todaysQuestions) {
-                // If no questions found for today, generate new questions
-                console.log('No daily questions found, generating new questions')
-                const questionIds = await generateNewQuestions();
-                todaysQuestions = { questionIDs: questionIds };
-            }
+    // Check if there is no session data or if the date in session data is different from today
+    //TODO: CHANGE TO CHECKING DATABASE FOR ACCOUNT INSTEAD OF USING SESSION DATA
+    if (!req.session.dailyChallengeData) {
+      const today = new Date(); //Setting todays time to 0s as MongoDB's Date field will always include a time (which we set to 0s when we created todays document)
+      today.setHours(0, 0, 0, 0);
+      let todaysQuestions = await DailyChallengeQuestions.findOne({ date: today }, 'questionIDs').lean();
 
-            // Update session data
-            req.session.dailyChallengeData = {
-                date: new Date(),
-                questionsRemaining: todaysQuestions.questionIDs,
-                todaysScore: 0,
-            };
-            await getNewQuestion(req);
-        }
-        let categories = await getQuestionCategories();
-        const initialDataToSend = {
-            questionsRemaining: req.session.dailyChallengeData.questionsRemaining,
-            todaysScore: req.session.dailyChallengeData.todaysScore,
-            currentQuestion: obfQuestion(req.session.dailyChallengeData.currentQuestion)
-        }   
-        const dailyChallengeData = ({dcd: initialDataToSend, categories: categories});
-        res.status(200).json(dailyChallengeData);
-    } catch (error) {
-        console.error('Error in requestDailyQuestions:', error);
-        res.status(500).json({ error: 'Failed to fetch daily challenge questions' });
+      if (!todaysQuestions) {
+        // If no questions found for today, generate new questions
+        console.log('No daily questions found, generating new questions')
+        const questionIds = await generateNewQuestions();
+        todaysQuestions = { questionIDs: questionIds };
+      }
+
+      // Update session data
+      req.session.dailyChallengeData = {
+        date: new Date(),
+        questionsRemaining: todaysQuestions.questionIDs,
+        todaysScore: 0,
+      };
+      await getNewQuestion(req);
     }
+    let categories = await getQuestionCategories();
+    const initialDataToSend = {
+      questionsRemaining: req.session.dailyChallengeData.questionsRemaining,
+      todaysScore: req.session.dailyChallengeData.todaysScore,
+      currentQuestion: obfQuestion(req.session.dailyChallengeData.currentQuestion)
+    }
+    const dailyChallengeData = ({ dcd: initialDataToSend, categories: categories });
+    res.status(200).json(dailyChallengeData);
+  } catch (error) {
+    console.error('Error in requestDailyQuestions:', error);
+    res.status(500).json({ error: 'Failed to fetch daily challenge questions' });
+  }
 });
 router.get('/request-question', async (req, res) => {
-    try {
-      const { statusCode, data } = await getNewQuestion(req);
-      res.status(statusCode).json(data);
-    } catch (error) {
-      console.error('Error in request-question:', error);
-      res.status(500).json({ status: "error", message: error.message });
-    }
-  });
+  try {
+    const { statusCode, data } = await getNewQuestion(req);
+    res.status(statusCode).json(data);
+  } catch (error) {
+    console.error('Error in request-question:', error);
+    res.status(500).json({ status: "error", message: error.message });
+  }
+});
+router.get('/get-user-history', async (req, res) => {
+  console.log(req.user._id)
+  try {
+    const userHistory = await DailyScore.find({ userId: req.user._id}).exec();
+    console.log(userHistory)
+    res.send(userHistory)
 
-
+  } catch (error) {
+    console.error("Error getting user history:", error)
+  }
+});
 router.post('/submit-answer', async (req, res) => {
-    const dailyChallengeData = req.session.dailyChallengeData;
-    if(dailyChallengeData) {
-      const answer = req.body.submittedAnswer;
-      if(answer === dailyChallengeData.currentQuestion.correctAnswer) dailyChallengeData.todaysScore+=1;
-  
-      res.send({ todaysScore: dailyChallengeData.todaysScore, correctAnswer: dailyChallengeData.currentQuestion.correctAnswer });
+  const dailyChallengeData = req.session.dailyChallengeData;
+  if (dailyChallengeData) {
+    const answer = req.body.submittedAnswer;
+    if (answer === dailyChallengeData.currentQuestion.correctAnswer) dailyChallengeData.todaysScore += 1;
 
-      updateDatabaseDailyChallengeScore(req);
+    res.send({ todaysScore: dailyChallengeData.todaysScore, correctAnswer: dailyChallengeData.currentQuestion.correctAnswer });
 
-    }
-  });
-  
+    updateDatabaseDailyChallengeScore(req);
+
+  }
+});
+
 
 export default router;
---
+++ frontend/src/components/DailyHistoryPanel/DailyHistoryPanel.js
@@ -0,0 +1,49 @@
+import styles from './DailyHistoryPanel.module.css';
+import { UserContext } from '../../contexts/UserContext';
+import React, { useState, useContext, useEffect} from 'react';
+import axios from 'axios';
+
+
+
+const DailyHistoryPanel = () => {
+
+    const { user, setUser } = useContext(UserContext);
+    const [userHistory, setUserHistory] = useState([]);
+    const [historyData, setHistoryData] = useState([]);
+
+
+    const getUserHistory = async () => {
+        console.log('getting user history')
+        try {
+            const response = await axios.get('/api/daily-challenge-routes/get-user-history');
+            setHistoryData(response.data);
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        if (user) getUserHistory();
+    }, [user])
+
+    useEffect(() => {   
+        if (historyData && historyData.length > 0) {
+            setUserHistory(historyData.map(historyEntry => ({
+                date: historyEntry.date.split("T")[0],
+                score: historyEntry.score
+            })));
+        }
+    }, [historyData]);
+
+    return (
+        <div className={styles.mainbody}>
+            {userHistory.map((entry, index) => (
+                <div key={index}>
+                    {entry.date} : {entry.score}
+                </div>
+            ))}
+        </div>
+    )
+}
+
+export default DailyHistoryPanel;
