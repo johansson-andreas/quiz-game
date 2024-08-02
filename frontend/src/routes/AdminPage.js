f7de6fc31700900c5f345b43aa2abd19a5356588
Aria Faraj <aria.faraj01@gmail.com>
2024-08-02 12:24:59 +0200

    new css to adminpage + updated with datatables

--- frontend/src/routes/AdminPage.js
+++ frontend/src/routes/AdminPage.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import "./styles/adminPageStyle.css";
+
 
 const AdminPage = () => {
   const [data, setData] = useState(null); // Storing fetched data
@@ -11,7 +13,8 @@ const AdminPage = () => {
     const fetchData = async () => {
       try {
         const response = await axios.get("/api/question-routes/request-new-questions");
-        setData(response.data.message); // Store fetched data
+        setData(response.data); // Store fetched data
+        console.log(response.data)
       } catch (err) {
         console.log(err)
         setError(err);
@@ -21,7 +24,7 @@ const AdminPage = () => {
     };
 
     fetchData(); // Call fetchData function
-  }, []); // Empty dependency array means this runs once on mount? Why?
+  }, []);
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error.message}</p>;
   if (!data) return <p>No Data available</p>;
@@ -29,7 +32,18 @@ const AdminPage = () => {
   return (
     <div>
       <h1>Admin Page</h1>
-      <pre>{JSON.stringify(data, null, 2)}</pre> {/* Render fetched data */}
+      <div className="data-cards">
+        {data.map(item => (
+          <div className="card" key={item._id}>
+            <h3>{item.text}</h3>
+            <p><strong>ID:</strong> {item._id}</p>
+            <p><strong>Correct Answer:</strong> {item.correctAnswer || 'None'}</p>
+            <p><strong>Incorrect Answers:</strong> {item.incorrectAnswers.join(', ')}</p>
+            <p><strong>Tags:</strong> {item.tags.join(', ')}</p>
+          </div>
+        ))}
+      </div>
+      
     </div>
   );
 };
