bcfbfeab211bde9fb18298b75df15c1439cd2432
Aria Faraj <aria.faraj01@gmail.com>
2024-08-02 10:20:24 +0200

    Added try and catch on handlesubmit for questionform

---
+++ backend/models/QuestionTest.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+const questionSchema = new Schema({
+  text: String,              // Question text
+  correctAnswer: String,     // Correct answer
+  incorrectAnswers: [String],// Array of incorrect answers
+  tags: [String]             // Array of tags
+});
+
+export const QuestionTest = mongoose.model('QuestionTest', questionSchema);
+
+
