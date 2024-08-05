43ce6bfc994e6870c041dd0eb4abd9ffe5c5eac4
Andreas <nevarrix@gmail.com>
2024-08-05 14:53:27 +0200

    Some cleanup. Start of multiplayer lobby. Transformed CommonJS modules to ES modules

--- backend/sockets/index.js
+++ backend/sockets/index.js
@@ -1,47 +1,23 @@
-const initialContact = require('./initialContact');
-const addQuestionToDB = require('./addQuestionToDB');
-const { submittedAnswer, getQueueByTags, getNewQuestionRequest } = require('./questionEvents');
+import initialContact from './initialContact.js';
+import { submittedAnswer, getQueueByTags, getNewQuestionRequest } from './questionEvents.js';
+import { createNewLobby } from './roomEvents.js';
 
+const sockets = (io) => {
 
-module.exports = function(io) {
+  const rooms = [];
 
   io.on('connection', (socket) => {
+
     const session = socket.request.session;
-    if (!session.clientData) {
-      session.clientData = {
-        unusedQuestions: [],
-        categories: {},
-        cachedQuestions: [],
-        username: '',
-        clientId: session.id,
-        scoreArray: {},
-        currentQuestion: {}
-      };
-      session.save();
-    }
-    console.log('Client', session.clientData.clientId, 'connected to the server');
 
-    submittedAnswer(socket, session);
-    getQueueByTags(socket, session);
-    getNewQuestionRequest(socket, session);
-
-    initialContact(socket, session);
- 
-    addQuestionToDB(socket);
+    initialContact(socket, rooms);
+    createNewLobby(socket, rooms);
     
 
-    socket.on('requestDailyChallengeQuestions', async () => {
-      console.log('test');
-      try {
-        const dailyQuestionsArray = await DailyChallengeQuestion.find();
-        console.log(dailyQuestionsArray);
-        socket.emit('sendingDailyChallengeArray', dailyQuestionsArray);
-      } catch (err) {
-        console.error('Failed to fetch daily challenge questions:', err);
-      }
-    });
     socket.on('disconnect', () => {
-      console.log('Client', session.clientData.clientId, 'disconnected from the server');
+      console.log('Client', socket.request.session.passport.user, 'disconnected from the server');
     });
   });
 };
+
+export default sockets;
