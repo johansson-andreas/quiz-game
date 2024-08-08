0eb60a201c90a3faa55524d94f0f6293966b2c0e
Andreas <nevarrix@gmail.com>
2024-08-08 11:19:28 +0200

    Extracted QuestionComponent from SP/DailyChallenge and made it its own component

--- frontend/src/routes/MultiPlayer.js
+++ frontend/src/routes/MultiPlayer.js
@@ -1,49 +1,53 @@
 import socket from "../Socket.js";
 import { useState, useEffect } from "react";
 
-const MultiPlayer = ({lobbyName}) => {
+const MultiPlayer = ({ lobbyName }) => {
+  const [users, setUsers] = useState([]);
 
-    const [users, setUsers] = useState([]);
+  useEffect(() => {
+    console.log("users", Object.keys(users));
+  }, [users]);
 
+  useEffect(() => {
+    socket.emit("getRoomInfo", lobbyName);
+    console.log("requesting room info");
 
-    useEffect(() => {
-      console.log(users);
+    socket.on("sendRoomInfo", (lobbyInfo) => {
+      console.log("room info", lobbyInfo);
+      setUsers(lobbyInfo.users);
+    });
 
-    }, [users])
+    socket.on("currentUsersInRoom", (currentUsers) => {
+      console.log("current users", currentUsers);
+      setUsers(currentUsers);
+    });
 
-    useEffect(() => {
-      socket.emit('getRoomInfo', lobbyName);
-      console.log('requesting room info')
+    return () => {
+      socket.emit("leaveRoom");
+      socket.off("getRoomInfo");
+      socket.off("sendRoomInfo");
+      socket.off("currentUsersInRoom");
+    };
+  }, []);
 
-      socket.on('sendRoomInfo', (lobbyInfo) => {
-        console.log('room info', lobbyInfo);
-        setUsers(Object.keys(lobbyInfo.users));
-      })
-      socket.on('currentUsersInRoom', (currentUsers) => {
-        console.log('current users', currentUsers);
-        setUsers(currentUsers.array.map(user => ({
-          username: user.username,
-          score: user.score
-        })));
-      });
-
-      return () => {
-        socket.off('getRoomInfo');
-        socket.off('sendRoomInfo')
-        socket.off('currentUsersInRoom')
-      };
-    }, [])
-
-
-    return (
-        <div>
-          <h1>MultiPlayer Page</h1>
-          <p>Lobby Name: {lobbyName}</p>
-          {users ? Object.keys(users).map(user => {
-            <div>{user}</div>
-
-          }) : ( <div></div>)}
-        </div>
-      );
-}
+  return (
+    <div>
+      <h1>MultiPlayer Page</h1>
+      <p>Lobby Name: {lobbyName}</p>
+      <div className="questionDiv"></div>
+      <div className="mpScoreDiv">
+        {users ? (
+          Object.keys(users).map((user) => (
+            <div key={user} className="userEntry">
+              <div className="usernameEntry">{users[user].username}</div>
+              <div className="scoreEntry">Score: {users[user].score}</div>
+            </div>
+          ))
+        ) : (
+          <div></div>
+        )}
+      </div>
+    </div>
+  );
+};
 export default MultiPlayer;
