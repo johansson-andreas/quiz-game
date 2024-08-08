28f628213acdad69f9580695090df8de6f75bc85
Andreas <nevarrix@gmail.com>
2024-08-08 13:23:07 +0200

    More work on frontend/backend multiplayer

--- backend/multiplayerSockets/roomEvents.js
+++ backend/multiplayerSockets/roomEvents.js
@@ -1,22 +1,48 @@
+import { getNewQuestionQueue } from "../routes/questionRouteUtils.js";
+import { Question } from "../models/Question.js";
+import { obfQuestion } from "../routes/questionRouteUtils.js";
+
 export const createNewLobby = (socket, rooms, io) => {
-  const username = socket.request.session.passport.user;
+  const username = socket.request.session.passport.user; 
 
   // Event handler to create a new lobby
-  socket.on("createNewLobby", (newLobbyInfo) => {
+  socket.on("createNewLobby", async (newLobbyInfo) => {
     const lobbyName = newLobbyInfo.lobbyName || generateLobbyName(rooms);
-    const newLobby = {
-      lobbyName: lobbyName,
-      chosenWinCon: newLobbyInfo.chosenWinCon,
-      winConNumber: newLobbyInfo.winConNumber,
-      password: newLobbyInfo.password,
-      questionTimer: newLobbyInfo.questionTimer,
-      users: {}, // Initialize users as an object
-      currentQuestion: {},
-    };
 
     if (!rooms[lobbyName]) {
+      const newLobby = {
+        lobbyName: lobbyName,
+        chosenWinCon: newLobbyInfo.chosenWinCon,
+        winConNumber: newLobbyInfo.winConNumber,
+        password: newLobbyInfo.password,
+        questionTimer: newLobbyInfo.questionTimer,
+        users: {}, // Initialize users as an object
+        currentQuestion: {},
+        questionQueue: [],
+        active: false,
+        host: username,
+      };
+
+      newLobby.questionQueue = await getNewQuestionQueue();
+
+      const randomIndex = Math.floor(
+        Math.random() * newLobby.questionQueue.length
+      );
+      const [newQuestionId] = newLobby.questionQueue.splice(randomIndex, 1); // Splices a question at randomIndex to randomize the order of the questions provided
+
+      try {
+        const newQuestion = await Question.findById(newQuestionId).lean();
+        console.log(newQuestion);
+        newLobby.currentQuestion = newQuestion;
+      } catch (error) {
+        console.error(
+          `Failed to fetch question by ID ${newQuestionId}:`,
+          error
+        );
+      }
+
       rooms[lobbyName] = newLobby;
-      console.log("Creating new room", newLobby);
+      console.log("Creating new room", newLobby.lobbyName);
       console.log("listOfCurrentRooms", Object.keys(rooms));
       socket.emit("createdNewLobby", { newLobby });
     } else {
@@ -26,7 +52,7 @@ export const createNewLobby = (socket, rooms, io) => {
   });
 
   socket.on("joinLobby", (joinLobbyInfo) => {
-    console.log("current rooms", rooms);
+    console.log("current rooms", Object.keys(rooms));
     const lobbyName = joinLobbyInfo.lobbyName;
     const password = joinLobbyInfo.joinLobbyPassword;
     console.log(username, "is trying to join lobby", lobbyName);
@@ -50,11 +76,32 @@ export const createNewLobby = (socket, rooms, io) => {
     }
   });
 
-  socket.on('getRoomInfo', (lobbyName) => {
-    console.log('sending room info', rooms[lobbyName])
-    socket.emit('sendRoomInfo', rooms[lobbyName])
+  socket.on("getRoomInfo", (lobbyName) => {
+    socket.emit("sendRoomInfo", {
+      users: rooms[lobbyName].users,
+      chosenWinCon: rooms[lobbyName].chosenWinCon,
+      winConNumber: rooms[lobbyName].winConNumber,
+      questionTimer: rooms[lobbyName].questionTimer,
+      active:rooms[lobbyName].active,
+      host:rooms[lobbyName].host,
+      currentQuestion: rooms[lobbyName].currentQuestion
+    });
+  });
 
-  }) 
+  socket.on("startGame", (lobbyName) => { 
+    rooms[lobbyName].active = true;
+    io.to(lobbyName).emit("startingGame", {
+      currentQuestion: obfQuestion(rooms[lobbyName].currentQuestion),
+      active:rooms[lobbyName].active
+    })
+
+    setTimeout(startTimer, rooms[lobbyName].questionTimer * 1000);
+  })
+};
+
+const startTimer = () => {
+  console.log(new Date().toISOString())
+
 };
 
 const generateLobbyName = (currentRooms) => {
--- frontend/src/routes/MultiPlayer.js
+++ frontend/src/routes/MultiPlayer.js
@@ -1,32 +1,123 @@
 import socket from "../Socket.js";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext, useRef} from "react";
+import { UserContext } from "../contexts/UserContext.js";
+import QuestionComponent from "../components/QuestionComponent/QuestionComponent.js";
+import ProgressBar from 'react-bootstrap/ProgressBar';
+
 
 const MultiPlayer = ({ lobbyName }) => {
   const [users, setUsers] = useState([]);
+  const [lobbyInfo, setLobbyInfo] = useState({
+    questionTimer: 0,
+  });  
+  const { user, setUser } = useContext(UserContext);
+  const [currentQuestion, setCurrentQuestion] = useState({});
+  const [questionText, setQuestionText] = useState(null);
+  const [correctAnswer, setCorrectAnswer] = useState(null);
+  const [questionIcons, setQuestionIcons] = useState([]);
+  const [question, setQuestion] = useState({});
+  const [options, setOptions] = useState([]);
+  const [answer, setAnswer] = useState("");
+  const [submittedAnswer, setSubmittedAnswer] = useState("");
+  const [totalQuestionsScore, setTotalQuestionsScore] = useState([0, 0]);
+  const [activeQuestion, setActive] = useState(true);
+  const [currentQuestionCategories, setCurrentQuestionCategories] = useState(
+    []
+  );
+  const [newQuestionCategories, setNewQuestionCategories] = useState([]);
+  const [scoreArray, setScoreArray] = useState({});
+  const [questionTags, setQuestionTags] = useState([]);
+  const [catCanvasShow, setCatCanvasShow] = useState(false);
+  const [hasMounted, setHasMounted] = useState(false);
+  const [triggeredOption, setTriggeredOption] = useState(null);
+  const [seconds, setSeconds] = useState(0);
+  const lobbyInfoRef = useRef(lobbyInfo);
+
+  const startGame = () => {
+    socket.emit("startGame", lobbyName);
+  };
+
+  const submitAnswer = (e) => {
+    setSubmittedAnswer(answer);
+  };
+
+  useEffect(() => {
+    console.log('new lobby info', lobbyInfo)
+
+    lobbyInfoRef.current = lobbyInfo;
+  }, [lobbyInfo]);
+
+  const nextQuestion = () => {
+    console.log("Next question");
+  };
+
+  const handleOptionChange = (e) => {
+    setAnswer(e.target.value);
+  };
+
+   const renderProgressBar = () => {
+    return <ProgressBar now={seconds} max={lobbyInfo.questionTimer} min={0}/>;
+  }
+
+  useEffect(() => {
+    console.log('setting timer to', seconds);
+    
+    if (seconds <= 0) {
+      console.log(new Date().toISOString());
+      return;
+    }
+    
+    const intervalId = setInterval(() => {
+      setSeconds(prevSeconds => Math.max(prevSeconds - 0.01, 0)); // Prevent going negative
+    }, 10);
+  
+    return () => clearInterval(intervalId);
+  }, [seconds]);
 
   useEffect(() => {
     console.log("users", Object.keys(users));
   }, [users]);
 
   useEffect(() => {
+    console.log('new timer', lobbyInfo.questionTimer)
+
+  }, [lobbyInfo.questionTimer])
+
+  useEffect(() => {
+
     socket.emit("getRoomInfo", lobbyName);
-    console.log("requesting room info");
-
-    socket.on("sendRoomInfo", (lobbyInfo) => {
-      console.log("room info", lobbyInfo);
+    console.log("Requesting room info");
+  
+    const handleRoomInfo = (lobbyInfo) => {
+      console.log("Received room info", lobbyInfo);
+      setLobbyInfo(lobbyInfo);
       setUsers(lobbyInfo.users);
-    });
-
-    socket.on("currentUsersInRoom", (currentUsers) => {
-      console.log("current users", currentUsers);
+    };
+  
+    const handleCurrentUsers = (currentUsers) => {
+      console.log("Current users", currentUsers);
       setUsers(currentUsers);
-    });
+    };
+  
+    const handleStartingGame = (updatedLobbyInfo) => {
+      setLobbyInfo((prevLobbyInfo) => {
+        let newLobbyInfo = { ...prevLobbyInfo };
+        newLobbyInfo.active = updatedLobbyInfo.active;
+        return newLobbyInfo;
+      });
+      setCurrentQuestion(lobbyInfo.currentQuestion);
+      setSeconds(lobbyInfo.questionTimer);
+    };
+  
+    socket.on("sendRoomInfo", handleRoomInfo);
+    socket.on("currentUsersInRoom", handleCurrentUsers);
+    socket.on("startingGame", handleStartingGame);
 
     return () => {
       socket.emit("leaveRoom");
-      socket.off("getRoomInfo");
-      socket.off("sendRoomInfo");
-      socket.off("currentUsersInRoom");
+      socket.off("sendRoomInfo", handleRoomInfo);
+      socket.off("currentUsersInRoom", handleCurrentUsers);
+      socket.off("startingGame", handleStartingGame);
     };
   }, []);
 
@@ -34,7 +125,30 @@ const MultiPlayer = ({ lobbyName }) => {
     <div>
       <h1>MultiPlayer Page</h1>
       <p>Lobby Name: {lobbyName}</p>
-      <div className="questionDiv"></div>
+      {lobbyInfo.active == false && lobbyInfo.host == user ? (
+        <>
+          {" "}
+          <button onClick={() => startGame()}>Start</button>
+        </>
+      ) : (
+        <div className="questionDiv">
+          <QuestionComponent
+            handleOptionChangeWrapper={handleOptionChange}
+            answer={answer}
+            question={currentQuestion}
+            questionIcons={questionIcons}
+            activeQuestion={activeQuestion}
+            nextQuestion={nextQuestion}
+            submitAnswer={submitAnswer}
+            submittedAnswer={submittedAnswer}
+            correctAnswer={correctAnswer}
+            triggeredOption={triggeredOption}
+            setTriggeredOption={setTriggeredOption}
+          />
+          {renderProgressBar()}
+        </div>
+      )}
+
       <div className="mpScoreDiv">
         {users ? (
           Object.keys(users).map((user) => (
