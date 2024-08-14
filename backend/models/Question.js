38bcaa27b942cc39a0610c700efdc9a7b61798ff
Aria Faraj <aria.faraj01@gmail.com>
2024-08-14 12:00:41 +0200

    updated questionScehma

--- backend/models/Question.js
+++ backend/models/Question.js
@@ -6,7 +6,9 @@ const questionSchema = new Schema({
   text: String,              // Question text
   correctAnswer: String,     // Correct answer
   incorrectAnswers: [String],// Array of incorrect answers
-  tags: [String]             // Array of tags
+  tags: [String],             // Array of tags
+  correctAnswerCount: { type: Number, default: 0 },
+  incorrectAnswerCount: { type: Number, default: 0 },
 });
 
 export const Question = mongoose.model('Question', questionSchema);
