7cd488ee8afaf3eadf87a545803c0cdf510075cc
Andreas <nevarrix@gmail.com>
2024-08-22 14:55:42 +0200

    start of new game mode.

--- frontend/src/components/GameModePanel/GameModePanel.js
+++ frontend/src/components/GameModePanel/GameModePanel.js
@@ -1,31 +1,18 @@
 import { useNavigate } from "react-router-dom";
-import { useEffect, usePreviousProps, useState } from "react";
 import styles from "./GameMode.module.css";
 
 const GameModePanel = () => {
   const navigate = useNavigate();
 
-  const spChoice = () => {
-    navigate("/sp");
-  };
-  const dailyChoice = () => {
-    navigate("/daily");
-  };
-  const questionFormChoice = () => {
-    navigate("/QuestionForm");
-  };
-  const mpChoice = () => {
-    navigate("MultiplayerLobby");
-  };
   const gameModeButtons = {
-    "Spela Själv": spChoice,
-    "Dagens Frågor": dailyChoice,
-    "Spela mot andra": mpChoice,
-    "Lägg till Frågor": questionFormChoice,
-    "Spela Själv2": spChoice,
-    "Dagens Frågor2": dailyChoice,
-    "Spela mot andra2": mpChoice,
-    "Lägg till Frågor2": questionFormChoice,
+    "Spela Själv": () => navigate("sp"),
+    "Dagens Frågor": () => navigate("daily"),
+    "Spela mot andra": () => navigate("MultiplayerLobby"),
+    "Lägg till Frågor": () => navigate("QuestionForm"),
+    "Ny": () => navigate("Gauntlet"),
+    "Dagens Frågor2": () => navigate("daily"),
+    "Spela mot andra2": () => navigate("MultiplayerLobby"),
+    "Lägg till Frågor2": navigate("QuestionForm"),
   };
 
   const gridSize = Math.ceil(Math.sqrt(Object.keys(gameModeButtons).length)); 
