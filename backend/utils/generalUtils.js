a5d911d18d10c97de003ee83ce216ce7f5d6a71b
Andreas <nevarrix@gmail.com>
2024-08-28 10:09:55 +0200

    Removed unnecessary console log outputs

--- backend/utils/generalUtils.js
+++ backend/utils/generalUtils.js
@@ -1,6 +1,4 @@
 export const randomizeArrayIndex = (array) => {
-    console.log('array length', array.length);
     const randomIndex = Math.floor(Math.random() * array.length);
-    console.log('index', randomIndex);
     return randomIndex;
   }
